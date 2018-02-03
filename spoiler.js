2f914627cfd98ba634a0af9c2ca1a63260388c90
Andrew971 <sordier.andrew@gmail.com>
2018-02-02 19:40:58 -0800

    Cleaning code

--- spoiler.js
+++ spoiler.js
@@ -1,9 +1,31 @@
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const request = require('request');
 
-let movie = process.argv[2]   
-let time = Number(process.argv[3]);
+// let movie = process.argv[2];
+// let time = Number(process.argv[3]);
 
+let movie= checkName();
+let time = checkNum();
+
+function checkName(name) {
+    name = process.argv[2];
+    if(name== null){
+        console.log('give me name')
+    }else {
+        return name
+    }
+
+}
+
+function checkNum(number) {
+    number = Number(process.argv[3]);
+    if(number=== 0 || number === NaN || number > 0 ){
+        console.log('give me number')
+        return
+    }else {
+        return number
+    }
+}
 
 request(`https://api.themoviedb.org/3/search/movie?api_key=c254f174e3ef7bd559ed74294ff10031&query=${movie}`, (err, response, body) => {
     const obj = JSON.parse(body)
